7295285d1af1aaf4bfad08fb8b15d98ea5eb8ae5
latkowska <latkowska.p@gmail.com>
2018-09-05 21:10:27 +0200

    test failed binFactoryError

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
