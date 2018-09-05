f9e6961b9d673578cd80e2b704b2e357a0182a87
latkowska <latkowska.p@gmail.com>
2018-09-05 12:12:21 +0200

    showtasks.bat file added

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call C:\Users\latko\KodillaDeveloper\tasks\runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runchrome
+echo.
+echo Running ...\showtasks.bat caused errors
+goto stop
+
+:runchrome
+@echo off
+cd "C:\Pliki programów(x86)\Google\Chrome\Application\"
+start chrome.exe http://localhost:8080/crud/v1/task/getTasks
+goto finish
+
+:stop
+echo.
+echo Stopped forced by errors
+
+:finish
+echo.
+echo Work is finished.
